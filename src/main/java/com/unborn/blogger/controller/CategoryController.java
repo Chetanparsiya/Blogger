a26e67316bbd77e1673d32cb780996b91cd9c7a1
Chetan Parsiya <chetanparsiya207@gmail.com>
2022-04-17 19:56:06 +0530

    Added Validations

--- src/main/java/com/unborn/blogger/controller/CategoryController.java
+++ src/main/java/com/unborn/blogger/controller/CategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -29,12 +30,12 @@ public class CategoryController {
     }
 
     @PostMapping
-    public  ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto){
+    public  ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto){
         return new ResponseEntity<>(categoryService.createCategory(categoryDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<CategoryDto> updateCategory(@RequestBody CategoryDto categoryDto, @PathVariable Long id){
+    public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto, @PathVariable Long id){
         return new ResponseEntity<>(categoryService.updateCategory(categoryDto, id),HttpStatus.OK);
     }
 
--- src/main/java/com/unborn/blogger/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/unborn/blogger/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,14 @@ package com.unborn.blogger.exceptions;
 import com.unborn.blogger.datatransferobject.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +21,17 @@ public class GlobalExceptionHandler {
         Boolean status = false;
         ApiResponse apiResponse = new ApiResponse(message,status);
         return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String ,String>> handleMethodArgsNotValidExceptionHandler(MethodArgumentNotValidException ex){
+        Map<String, String> resp = new HashMap<String,String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String field = ((FieldError)(error)).getField();
+            String errorMessage = error.getDefaultMessage();
+            resp.put(field, errorMessage);
+        });
+        return new ResponseEntity<Map<String,String>>(resp, HttpStatus.BAD_REQUEST);
+        
     }
 }
